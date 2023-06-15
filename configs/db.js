57d34ab8109092c8a3f9f80eefa3429300ce62f7
Amanmandal-M <amanmandal644@gmail.com>
2023-06-15 23:41:25 +0530

    Fixing Bugs

--- configs/db.js
+++ configs/db.js
@@ -7,6 +7,7 @@ const SeqConnection = new Sequelize(
         process.env.DB_PASSWORD,
         {
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
             dialect:"mysql",
         }
 )
