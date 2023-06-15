b337e6bb92213187ad9ca45ed3af880e66520e06
Amanmandal-M <amanmandal644@gmail.com>
2023-06-15 23:49:33 +0530

    Fixed Bugs

--- configs/db.js
+++ configs/db.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const SeqConnection = new Sequelize( 
-        process.env.DB_NAME,  //Database Name
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
+        process.env.MYSQLDATABASE,  //Database Name
+        process.env.MYSQLUSER,
+        process.env.MYSQLPASSWORD,
         {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
+            host: process.env.MYSQLHOST,
+            port: process.env.MYSQLPORT,
             dialect:"mysql",
         }
 )
