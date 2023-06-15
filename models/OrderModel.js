005895f1acaac422630f226adba87a4249273911
Amanmandal-M <amanmandal644@gmail.com>
2023-06-16 00:52:24 +0530

    Updated Orders Model

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -25,6 +25,16 @@ const OrderModel = SeqConnection.define(
               model: 'UserModel',
               key: 'id'
             }
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+          },
+          updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
           }
     }
 )
